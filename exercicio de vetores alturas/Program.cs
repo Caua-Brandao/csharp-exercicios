3e12fcfe0c82d3925e959c6cf77669ab05b3a4b5
Caua <cauabrandaocosta1503@gmail.com>
2026-04-04 21:39:52 -0300

    Adicionando mais um exercicio de vetores

---
+++ exercicio de vetores alturas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Número de pessoas: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Pessoa[] vect = new Pessoa[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Altura: ");
+            double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vect[i] = new Pessoa(nome, altura);
+        }
+        double soma = 0;
+        for (int i = 0; i < n; i++)
+        {
+            soma += vect[i].Altura;
+        }
+        double media = soma / n;
+        Console.WriteLine("\nMédia: " + media.ToString("F2", CultureInfo.InvariantCulture));
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (vect[i].Altura < media)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("Pessoas abaixo da média: " + count);
+
+        Console.WriteLine("\nNomes:");
+        for (int i = 0; i < n; i++)
+        {
+            if (vect[i].Altura < media)
+            {
+                Console.WriteLine(vect[i].Nome);
+            }
+        }
+    }
+}
+
+class Pessoa
+{
+    public string Nome { get; set; }
+    public double Altura { get; set; }
+
+    public Pessoa(string nome, double altura)
+    {
+        Nome = nome;
+        Altura = altura;
+    }
+}
