9a3a6f739e1291d84d67e36b7f6a576f2744fdc3
Caua <cauabrandaocosta1503@gmail.com>
2026-04-10 22:10:10 -0300

    Adicionando exercicios de composicao

---
+++ Composicao/ExercicioComposicao/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace Composicao.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        { 
+        }
+
+        public Department(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Composicao/ExercicioComposicao/Entities/HourContract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Composicao.Entities
+{
+    internal class HourContract
+    {
+        public DateTime Date { get; set; }
+
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            this.Date = date;
+            this.ValuePerHour = valuePerHour;
+            this.Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
