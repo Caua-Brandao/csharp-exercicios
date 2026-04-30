860765f4565f85cf08216c98b4e8222002dad61f
Caua <cauabrandaocosta1503@gmail.com>
2026-04-29 21:53:40 -0300

    Adicionando exercício de manipulação de arquivos

---
+++ Manipulação de arquivos/FilesTest/Product.cs
@@ -0,0 +1,22 @@
+﻿
+namespace FilesTest
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int  Quanty { get; set; } 
+
+        public Product(string name, double price, int quanty)
+        {
+            Name = name;
+            Price = price;
+            Quanty = quanty;
+        }
+
+        public double Total()
+        {
+            return Price * Quanty;
+        }
+    }
+}
