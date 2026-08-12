8183278781182759f119d9857bf30885060647a7
Caua <cauabrandaocosta1503@gmail.com>
2026-08-12 07:21:04 -0300

    Adicionando exercício de conjuntos

---
+++ Conjuntos/UserControl/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UserControl.Entities;
+
+namespace UserControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string sourcePath = @"C:\Users\cauab\OneDrive\Desktop\userControl.txt";
+                HashSet<LogRecord> users = new HashSet<LogRecord>();
+                using (StreamReader sr = new StreamReader(sourcePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        users.Add(new LogRecord(line[0], DateTime.Parse(line[1])));
+                    }
+                }
+                Console.WriteLine("Total users: " + users.Count);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred" + e.Message);
+            }
+        }
+    }
+}
